5b1dde9aa2ae35c743423060e5830a4b0f9f0c59
Ocean15 <dustin.hoffmann@icloud.com>
2018-03-16 15:33:14 +0100

    Fixed dapp settings for regtest

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,56 +19,56 @@ port=${_NODE_PORT}" > data/.namecoin/namecoin.conf
 DAPP_SETTINGS='{
   "app": {
     "types": ['
-if [ $DAPP_SEND = true ]; then 
+if [ $DAPP_SEND = true ]; then
   DAPP_SETTINGS=$DAPP_SETTINGS'"send"'
-  if [ $DAPP_CONFIRM = true ] || [ $DAPP_VERIFY = true ]; then 
+  if [ $DAPP_CONFIRM = true ] || [ $DAPP_VERIFY = true ]; then
 	DAPP_SETTINGS=$DAPP_SETTINGS','
   fi
 fi
-if [ $DAPP_CONFIRM = true ]; then 
+if [ $DAPP_CONFIRM = true ]; then
   DAPP_SETTINGS=$DAPP_SETTINGS'"confirm"'
-  if [ $DAPP_VERIFY = true ]; then 
+  if [ $DAPP_VERIFY = true ]; then
 	DAPP_SETTINGS=$DAPP_SETTINGS','
   fi
 fi
-if [ $DAPP_VERIFY = true ]; then 
+if [ $DAPP_VERIFY = true ]; then
   DAPP_SETTINGS=$DAPP_SETTINGS'"verify"'
 fi
 DAPP_SETTINGS=$DAPP_SETTINGS']
   },'
-  
-  
-if [ $DAPP_SEND = true ]; then 
+
+
+if [ $DAPP_SEND = true ]; then
   DAPP_SETTINGS=$DAPP_SETTINGS'"send": {
 	"namecoin": {
       "host": "localhost",
-      "port": "'$RPC_PORT'",
+      "port": "'$_RPC_PORT'",
       "username": "'$RPC_USER'",
       "password": "'$RPC_PASSWORD'"
     }
   }'
-  if [ $DAPP_CONFIRM = true ] || [ $DAPP_VERIFY = true ]; then 
+  if [ $DAPP_CONFIRM = true ] || [ $DAPP_VERIFY = true ]; then
 	DAPP_SETTINGS=$DAPP_SETTINGS','
   fi
 fi
-if [ $DAPP_CONFIRM = true ]; then 
+if [ $DAPP_CONFIRM = true ]; then
   DAPP_SETTINGS=$DAPP_SETTINGS'"confirm": {
 	"namecoin": {
       "host": "localhost",
-      "port": "'$RPC_PORT'",
+      "port": "'$_RPC_PORT'",
       "username": "'$RPC_USER'",
       "password": "'$RPC_PASSWORD'"
     }
   }'
-  if [ $DAPP_VERIFY = true ]; then 
+  if [ $DAPP_VERIFY = true ]; then
 	DAPP_SETTINGS=$DAPP_SETTINGS','
   fi
 fi
-if [ $DAPP_VERIFY = true ]; then 
+if [ $DAPP_VERIFY = true ]; then
   DAPP_SETTINGS=$DAPP_SETTINGS'"verify": {
 	"namecoin": {
       "host": "localhost",
-      "port": "'$RPC_PORT'",
+      "port": "'$_RPC_PORT'",
       "username": "'$RPC_USER'",
       "password": "'$RPC_PASSWORD'"
     }
