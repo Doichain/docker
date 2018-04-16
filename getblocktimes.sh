b1c59fe410ef065df6a97011095d959dc0b39e70
Nico Krause <nico@le-space.de>
2018-04-16 13:34:49 +0200

    added block duration

--- getblocktimes.sh
+++ getblocktimes.sh
@@ -3,16 +3,18 @@
 LASTBLOCK=$(namecoin-cli getchaintips | jq '.[0].height' | sed 's/\"//g')
 echo "lastblock:"$LASTBLOCK
 LAST_TIME=0
-for ((z=LASTBLOCK-100;z<=LASTBLOCK;z++))
+for ((z=LASTBLOCK-50;z<=LASTBLOCK;z++))
 do
  THIS_HASH=$(namecoin-cli getblockhash $z)
- BLOCK_UNIXTIME=$(namecoin-cli getblock $THIS_HASH | jq '.time')
+ THIS_BLOCK=$(namecoin-cli getblock $THIS_HASH)
+ BLOCK_UNIXTIME=$(echo $THIS_BLOCK | jq '.time')
+ #BLOCK_UNIXTIME=$(namecoin-cli getblock $THIS_HASH | jq '.time')
  DURATION_UNIXTIME=$(($BLOCK_UNIXTIME-$LAST_TIME))
- #echo $DURATION_UNIXTIME
+ echo $THIS_HASH
  DURATION_TIME=$(date -d @$DURATION_UNIXTIME | cut -c 12-20)
-
+ DIFFICULTY=$(echo $THIS_BLOCK | jq '.difficulty')
  LAST_TIME=$BLOCK_UNIXTIME
 
  BLOCK_TIME=$(date -d @$BLOCK_UNIXTIME)
- echo $z" "$THIS_HASH" "$BLOCK_UNIXTIME" "$BLOCK_TIME" :"$DURATION_TIME
+ echo $z" "$THIS_HASH" "$BLOCK_UNIXTIME" "$BLOCK_TIME" :"$DURATION_TIME" : "$DIFFICULTY
 done
