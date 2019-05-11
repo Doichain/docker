95a948c940ea1da5ba6e4033ce1e5b6d188df67e
Nico Krause <nico@le-space.de>
2019-05-11 19:06:38 +0500

    fixed settings.json now as env variable

--- dapp-start.sh
+++ dapp-start.sh
@@ -4,7 +4,8 @@
 #echo 'starting creating bundle $DAPP_HOST:$DAPP_PORT' && cd dapp && rm -rf build
 #meteor build build/ --architecture os.linux.x86_64 --directory #--server $DAPP_HOST:$DAPP_PORT
 #echo 'finished creating bundle' && cd build/bundle/programs/server && npm install && cd -
-#echo 'starting doichain dapp via node' && export METEOR_SETTINGS=$(cat /home/doichain/data/dapp/settings.json) && 
+#echo 'starting doichain dapp via node' && 
+export METEOR_SETTINGS=$(cat /home/doichain/data/dapp/settings.json)
 export PORT=$DAPP_PORT
 export ROOT_URL=http://$DAPP_HOST:$DAPP_PORT
 
